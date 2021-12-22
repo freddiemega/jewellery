eb4c9930fd5624ff45d2a7b11ec024c2de6a1b64
Юрий <turbofan2000@mail.ru>
2021-12-22 10:51:35 +0300

    Всё вроде бы

--- source/js/script.js
+++ source/js/script.js
@@ -20,208 +20,141 @@ navToggle.addEventListener('click', function () {
 
 // аккордеон
 var faqList = document.querySelector('.faq__list');
-var faqListItems = faqList.getElementsByClassName("faq__item");
 
-faqList.querySelectorAll('.faq__icon-opened').forEach(n => n.classList.remove('faq__icon-opened--nojs'));
-faqList.querySelectorAll('.faq__icon-closed').forEach(n => n.classList.remove('faq__icon-closed--nojs'));
-faqList.querySelectorAll('.faq__answer').forEach(n => n.classList.remove('faq__answer--nojs'));
+if (faqList) {
+  var faqListItems = faqList.getElementsByClassName("faq__item");
 
-for (var i = 0; i < faqListItems.length; i++) {
-  faqListItems[i].querySelector('.faq__button').addEventListener('click', setHandler(i),false);
+  faqList.querySelectorAll('.faq__icon-opened').forEach(n => n.classList.remove('faq__icon-opened--nojs'));
+  faqList.querySelectorAll('.faq__icon-closed').forEach(n => n.classList.remove('faq__icon-closed--nojs'));
+  faqList.querySelectorAll('.faq__answer').forEach(n => n.classList.remove('faq__answer--nojs'));
+
+  for (var i = 0; i < faqListItems.length; i++) {
+    faqListItems[i].querySelector('.faq__button').addEventListener('click', setHandler(i),false);
+    }
+    function setHandler(i){
+      return function (e) {
+      faqListItems[i].querySelector('.faq__answer').classList.toggle('faq__answer--show');
+      faqListItems[i].querySelector('.faq__icon-closed').classList.toggle('faq__icon-closed--show');
+      faqListItems[i].querySelector('.faq__icon-opened').classList.toggle('faq__icon-opened--show');
+    };
   }
-  function setHandler(i){
-    return function (e) {
-    faqListItems[i].querySelector('.faq__answer').classList.toggle('faq__answer--show');
-    faqListItems[i].querySelector('.faq__icon-closed').classList.toggle('faq__icon-closed--show');
-    faqListItems[i].querySelector('.faq__icon-opened').classList.toggle('faq__icon-opened--show');
-  };
 }
 
-// карусель
-let mouse = {
-  pressed: false
-  , lastX: 0}
-let ul = null;
+// аккордеон фильтры
+var filtersList = document.querySelector('.filter__list');
 
-var carousel = document.querySelector('.slider');
-var buttonPrev = carousel.querySelector('.slider__button--prev');
-var buttonNext = carousel.querySelector('.slider__button--next');
-// вычисляем количество элементов в слайдере
-var numberSliderElement = carousel.querySelectorAll('.slider__item').length;
-// получаем элемент в который надо вставить пагинацию
-var slider = document.querySelector('.slider');
-var currentPage = 1;
-pagination (currentPage , numberSliderElement);
+if (filtersList) {
+  var filterListItems = filtersList.getElementsByClassName("filter__item");
 
-var formPagination = slider.querySelector('.slider__pagination');
-//console.log(formPagination);
-if (getAdaptabilityOption() == 'desktop') {
-  var maxPage = numberSliderElement / 4;
-} else {
-  var maxPage = numberSliderElement / 2;
-}
+  filtersList.querySelectorAll('.filter__icon-opened').forEach(n => n.classList.remove('filter__icon-opened--nojs'));
+  filtersList.querySelectorAll('.filter__icon-closed').forEach(n => n.classList.remove('filter__icon-closed--nojs'));
+  filtersList.querySelectorAll('.checkbox-list').forEach(n => n.classList.remove('checkbox-list--nojs'));
 
-buttonPrev.addEventListener('click', function () {
-  move('right', 1);
-});
-
-buttonNext.addEventListener('click', function () {
-  move('left', 1);
-});
-
-function mouseDown(e) {
-  mouse.pressed = true;
-  ul.style.transition = "all 0s";
-  mouse.lastX = e.clientX;
-}
-
-function mouseUp(e) {
-  if (!mouse.pressed) return;
-  mouse.pressed = false;
-  ul.style.transition = "all 0.5s";
-  let inFrame = document.body.querySelector('.slider__wrapper').getBoundingClientRect();
-  let left = ul.getBoundingClientRect().x - inFrame.x;
-  left = Math.round(left / 130) * 130;
-  let maxLeft = document.body.querySelector('.slider__list li:last-child');
-  maxLeft = -(maxLeft.offsetLeft + maxLeft.offsetWidth - inFrame.width);
-  if (left < maxLeft) left = maxLeft;
-  if (left > 0) left = 0;
-  ul.style.left = left + 'px';
-}
-
-function mouseMove(e) {
-  if (!mouse.pressed) return;
-  if (e.buttons == 0) return;
-  let inFrame = document.body.querySelector('.slider__wrapper').getBoundingClientRect();
-  let left = ul.getBoundingClientRect().x - inFrame.x;
-  ul.style.left = left + e.clientX - mouse.lastX + 'px';
-  //console.log(e);
-  mouse.lastX = e.clientX;
-}
-
-function move(direction, numberOfSlideGroups) {
-  let inFrame = document.body.querySelector('.slider__wrapper').getBoundingClientRect();
-  let left = ul.getBoundingClientRect().x - inFrame.x;
-  let maxLeft = document.body.querySelector('.slider__list li:last-child');
-  maxLeft = -(maxLeft.offsetLeft + maxLeft.offsetWidth - inFrame.width);
-  switch (direction) {
-  case 'left':
-    left = left - inFrame.width - 30;
-    left = left * numberOfSlideGroups;
-    ul.style.left = left + 'px';
-    if (left < maxLeft) {
-      ul.style.left = maxLeft - 200 + 'px';
-      setTimeout(() => {
-        ul.style.left = maxLeft + 'px'
-      }, 400)
+  for (var i = 0; i < filterListItems.length; i++) {
+    filterListItems[i].querySelector('.filter__button').addEventListener('click', setHandler(i),false);
     }
-    if (currentPage < maxPage) {
-      currentPage = currentPage + numberOfSlideGroups;
-    }
-    pagination (currentPage, numberSliderElement);
-    break;
-  case 'right':
-    left = left + (inFrame.width + 30) * numberOfSlideGroups;
-
-    ul.style.left = left + 'px';
-    if (left > 0) {
-      ul.style.left = 200 + 'px';
-      setTimeout(() => {
-        ul.style.left = 0 + 'px'
-      }, 400)
-    }
-    if (currentPage > 1) {
-      currentPage = currentPage - numberOfSlideGroups;
-    }
-    pagination (currentPage, numberSliderElement);
-    break;
+    function setHandler(i){
+      return function (e) {
+        e.preventDefault();
+        filterListItems[i].querySelector('.checkbox-list').classList.toggle('checkbox-list--show');
+        filterListItems[i].querySelector('.filter__icon-closed').classList.toggle('filter__icon-closed--show');
+        filterListItems[i].querySelector('.filter__icon-opened').classList.toggle('filter__icon-opened--show');
+    };
   }
-};
-// отметить картинки для удобства разработки
-// этот код может быть удалён по вашему усмотрению
-
-/*
-let j = 1;
-for (let li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;right:65px;bottom:5px">${j}</span>`);
-  j++;
 }
-*/
-carousel.onmousedown = mouseDown;
-carousel.onmouseup = mouseUp;
-carousel.onmouseout = mouseUp;
-carousel.onmousemove = mouseMove;
-ul = slider.querySelector('.slider__list');
 
-function pagination (currentPage, totalSlides) {
-  // удаляем старую пагинацию, если она существует
-  if (slider.querySelector('.slider__pagination')) {
-    slider.querySelector('.slider__pagination').remove();
-  }
-  //создаём форму-список
-  var paginationContainer = document.createElement('form');
-  paginationContainer.name = 'pagination-form';
-  // добавляем элементу списка класс
-  paginationContainer.classList.add('slider__pagination');
+// кнопка filter
+var buttonToShowFilter = document.querySelector('.catalog__button');
+var blockFilter = document.querySelector('.filter');
+if (blockFilter) {
+  var buttonToCloseFilter = blockFilter.querySelector('.filter__button-close')
 
-  if (getAdaptabilityOption() == 'mobile') {
-    var paginationContainer = document.createElement('p');
-    // добавляем элементу списка класс
-    paginationContainer.classList.add('slider__pagination');
-    // добавляем значение
-    paginationContainer.textContent = '1  of  ' + (totalSlides / 2);
-  } else {
-    if (getAdaptabilityOption() == 'desktop') {
-      var totalPages = totalSlides / 4;
-    } else {
-      // 'tablet' case
-      var totalPages = totalSlides / 2;
-    }
-    for (var i = 1; i <= totalPages; i++) {
-      // создаём кнопку пагинации
-      var buttonElement = document.createElement('button');
-      buttonElement.type = 'button';
-      // задаём кнопке параметр name
-      buttonElement.name = 'slider-button-' + i;
-      // добавляем кнопке класс
-      buttonElement.classList.add('slider__pagination-button');
-      //
-      if (i == currentPage) {
-        buttonElement.classList.add('slider__pagination-button--active');
-        buttonElement.disabled = true;
+  blockFilter.classList.remove('filter--nojs');
+  buttonToShowFilter.classList.remove('catalog__button--nojs');
+  buttonToCloseFilter.classList.remove('filter__button-close--nojs');
+
+  buttonToShowFilter.addEventListener('click', function (event) {
+    blockFilter.classList.toggle('filter--closed');
+    document.body.style.overflow = 'hidden';
+  });
+
+  // кнопка закрыть filter
+  buttonToCloseFilter.addEventListener('click', function (event) {
+    document.body.style.overflow = 'visible';
+    blockFilter.classList.toggle('filter--closed');
+  });
+}
+
+// модальное окно
+
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalCall = modalOverlay.querySelector('.modal-login');
+var buttonToOpenModal = document.querySelector('.page-header__login');
+var buttonToOpenModalTabletMobile = document.querySelector('.page-header__login-link');
+var buttonToCloseModal = document.querySelector('.modal-login__button-close');
+
+var userEmailField = modalCall.querySelector('[name="user-email"]');
+
+buttonToOpenModal.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalOverlay.classList.remove('visually-hidden');
+  modalOverlay.classList.add('modal-overlay--open');
+  modalCall.classList.add('modal-login--open');
+  userEmailField.focus();
+  document.body.style.overflow = 'hidden';
+
+  if (modalCall.classList.contains('modal-login--open')) {
+    window.addEventListener('click', function (el) {
+      if (el.target === modalOverlay) {
+        document.body.style.overflow = 'visible';
+        modalOverlay.classList.remove('modal-overlay--open');
+        modalCall.classList.remove('modal-login--open');
       }
-      // добавляем кнопке значение
-      buttonElement.textContent = i;
-
-      buttonElement.addEventListener('click', function (event) {
-        var numberOfCurrentButton = paginationContainer.querySelector('.slider__pagination-button--active').textContent;
-        if([...event.target.classList].includes('slider__pagination-button')) {
-          var numberOfPressedButton = event.target.textContent;
-        }
-        if (numberOfPressedButton > numberOfCurrentButton) {
-          var rangeToMoveLeft = numberOfPressedButton - numberOfCurrentButton;
-            move('left', rangeToMoveLeft);
-        } else {
-          var rangeToMoveRight= numberOfCurrentButton - numberOfPressedButton;
-            //console.log(rangeToMoveRight);
-            move('right', rangeToMoveRight);
-        }
-      });
-
-      // вставляем кнопку внутрь <form>
-      paginationContainer.appendChild(buttonElement);
-    }
+    });
+    window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        document.body.style.overflow = 'visible';
+        modalOverlay.classList.remove('modal-overlay--open');
+        modalCall.classList.remove('modal-login--open');
+      }
+    });
+    buttonToCloseModal.addEventListener('click', function () {
+      document.body.style.overflow = 'visible';
+      modalOverlay.classList.remove('modal-overlay--open');
+      modalCall.classList.remove('modal-login--open');
+    });
   }
-  slider.append(paginationContainer);
-}
 
-function getAdaptabilityOption() {
-  if (window.innerWidth >= 1024) {
-    return 'desktop';
-  } if (window.innerWidth < 1024 && window.innerWidth >= 768) {
-    return 'tablet';
-  } else {
-    return 'mobile';
+});
+
+buttonToOpenModalTabletMobile.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalOverlay.classList.remove('visually-hidden');
+  modalOverlay.classList.add('modal-overlay--open');
+  modalCall.classList.add('modal-login--open');
+  userEmailField.focus();
+  document.body.style.overflow = 'hidden';
+
+  if (modalCall.classList.contains('modal-login--open')) {
+    window.addEventListener('click', function (el) {
+      if (el.target === modalOverlay) {
+        document.body.style.overflow = 'visible';
+        modalOverlay.classList.remove('modal-overlay--open');
+        modalCall.classList.remove('modal-login--open');
+      }
+    });
+    window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        document.body.style.overflow = 'visible';
+        modalOverlay.classList.remove('modal-overlay--open');
+        modalCall.classList.remove('modal-login--open');
+      }
+    });
+    buttonToCloseModal.addEventListener('click', function () {
+      document.body.style.overflow = 'visible';
+      modalOverlay.classList.remove('modal-overlay--open');
+      modalCall.classList.remove('modal-login--open');
+    });
   }
-}
+
+});
